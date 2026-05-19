85545ef833af78351fb7051c44f077478972f4af
12037 <15079254622@163.com>
2026-05-19 22:15:01 +0800

    优化显示效果

--- photo-choice/src/main/java/com/google/photochoice/ui/widget/SquareFrameLayout.kt
+++ photo-choice/src/main/java/com/google/photochoice/ui/widget/SquareFrameLayout.kt
@@ -2,19 +2,21 @@ package com.google.photochoice.ui.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View.MeasureSpec
 import android.widget.FrameLayout
 
 /**
- * 始终保持 1:1 宽高比的 FrameLayout。
- * 网格缩略图的根布局使用此 View，按测量得到的 width 强制 height 与之相等。
+ * 始终保持 1:1 宽高比的 FrameLayout。网格缩略图根布局使用，按列宽强制为正方形。
  */
 class SquareFrameLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+    defStyleAttr: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val squareSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY)
+        super.onMeasure(squareSpec, squareSpec)
     }
 }
